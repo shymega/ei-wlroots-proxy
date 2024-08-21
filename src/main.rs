facb6dd789730d5882b6500c0884914bd1a629a2
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-21 20:15:18 +0100

    feat: Demo code that shows supported protocols

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,34 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use wayland_client::{Connection, Dispatch, QueueFreezeGuard, protocol::{wl_registry}};
+
+struct AppData;
+
+impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
+    fn event(
+            _state: &mut Self,
+            _proxy: &wl_registry::WlRegistry,
+            event: <wl_registry::WlRegistry as wayland_client::Proxy>::Event,
+            _data: &(),
+            _conn: &Connection,
+            _qhandle: &wayland_client::QueueHandle<Self>,
+        ) {
+
+            if let wl_registry::Event::Global { name, interface, version } = event {
+                println!("[{}] {} (v{})", name, interface, version);
+            }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conn = Connection::connect_to_env().unwrap();
+    let display = conn.display();
+
+    let mut event_queue = conn.new_event_queue();
+    let queue_handle = event_queue.handle();
+
+    _ = display.get_registry(&queue_handle, ());
+
+    event_queue.roundtrip(&mut AppData).unwrap();
 }
