3853498bf1aaf4551ac4aa5ee361cb1150186712
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-21 20:06:17 +0100

    chore: Run `nixpkgs-fmt` on `shell.nix`

--- shell.nix
+++ shell.nix
@@ -4,10 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-(import (
-  fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
-    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
-) {
-  src =  ./.;
-}).shellNix
+(import
+  (
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+      sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    }
+  )
+  {
+    src = ./.;
+  }).shellNix
