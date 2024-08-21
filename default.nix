c922baf50d2ab90bd62ae84b025b0b0abe01b024
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-21 19:48:21 +0100

    feat: Bring project in line with REUSE

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
+# SPDX-FileCopyrightText: 2024 The Input Leap Team
 # SPDX-FileCopyrightText: 2024 The ei-wlroots-proxy Developers
 #
 # SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 (import
   (
