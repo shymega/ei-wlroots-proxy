1347c1b0a4ac446de72320fb9a513fe278926280
Dom Rodriguez <shymega@shymega.org.uk>
2025-04-07 20:43:09 +0100

    feat: Add Dom Rodriguez as copyright holder
    - As well as Input Leap, but this repository will move to my own
      personal namespace. Input Leap is welcome to fork.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- build-aux/nix/default.nix
+++ build-aux/nix/default.nix
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2024 The Input Leap Developers
 # SPDX-FileCopyrightText: 2024 The ei-wlroots-proxy Developers
+# SPDX-FileCopyrightText: 2025 Dom Rodriguez
 #
-# SPDX-License-Identifier: Apache-2.0
 # SPDX-License-Identifier: Apache-2.0
 
 { lib
--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 # SPDX-FileCopyrightText: 2024 The Input Leap Developers
+# SPDX-FileCopyrightText: 2025 Dom Rodriguez
 #
 # SPDX-License-Identifier: Apache-2.0
 
